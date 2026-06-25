e132435dc0b112bcd706bf0c63afa02151845669
Theo Snyman <theosnyman.dev@gmail.com>
2026-06-25 21:49:23 +0200

    scaffold project

---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2026-06-24", // project scaffold date
+  devtools: { enabled: true },
+});
