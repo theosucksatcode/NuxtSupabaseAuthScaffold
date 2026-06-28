55c8c235022d86f92fe191a7f926c6bcf9a76d4b
Theo Snyman <theosnyman.dev@gmail.com>
2026-06-28 16:21:31 +0200

    comments for my later self

--- app/middleware/guest.ts
+++ app/middleware/guest.ts
@@ -1,3 +1,4 @@
+// used by 3 of the auth routes whilst confirm handles its own implementation internally
 export default defineNuxtRouteMiddleware(() => {
   if (useSupabaseUser().value) return navigateTo("/app/dashboard");
 });
--- app/pages/auth/confirm.vue
+++ app/pages/auth/confirm.vue
@@ -5,5 +5,6 @@
 <script setup lang="ts">
 definePageMeta({
   layout: "centered",
+  // confirm kind of has guest middleware functionality in the component self... does not follow how other auth components handle it
 });
 </script>
